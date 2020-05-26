a29651af9df02c5b8bd6be293adab2411163bd20
Raz Malka <30453323+RazMalka@users.noreply.github.com>
2020-05-26 19:07:44 +0300

    v0.02

--- svm.py
+++
@@ -1,7 +0,0 @@
-# Data Mining and Machine Learning 2020
-# Group Number 2    ->  [Raz Malka | Shoham Yamin | Raz Itzhak Afriat]
-# Research Subject  ->  One-Class SVMs for Document Classification
-
-# This file is meant to provide implementation to svm versions appropriate
-# for one-class classification in the context of information retrieval.
-
